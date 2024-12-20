74ab22edcce18ff812dec6e400ab8dfe6e8abff2
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2024-12-20 11:55:22 +0100

    adds flyway db migrations; fixes PDF scraping on FFG

--- scraping-demo/src/main/java/org/example/Main.java
+++ scraping-demo/src/main/java/org/example/Main.java
@@ -1,50 +1,14 @@
 package org.example;
 
-import com.microsoft.playwright.*;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import org.flywaydb.core.Flyway;
 
 public class Main {
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
-            BrowserContext context = browser.newContext();
-            Page page = context.newPage();
+        Flyway flyway = Flyway.configure()
+                .dataSource("jdbc:postgresql://localhost:5432/grantscout-scraping", "gs", "Password12345678")
+                .load();
+        flyway.migrate();
 
-            String url = "https://www.ffg.at/ausschreibung/AustrianLifeSciences-2024-2026";
-            page.navigate(url);
-
-            // Extract all PDF links
-            List<Locator> pdfLinks = new ArrayList<>();
-            page.locator("section.main a").all().forEach(link -> {
-                String linkDestination = link.getAttribute("href");
-                if(linkDestination == null) {
-                    return;
-                }
-
-                if(linkDestination.startsWith("https://fdoc.ffg.at/") || linkDestination.endsWith(".pdf")) {
-                    pdfLinks.add(link);
-                }
-            });
-
-            // Make dir download in user.dir if not exists
-            Path downloadDir = Paths.get(System.getProperty("user.dir"), "download");
-            if (!downloadDir.toFile().exists()) {
-                boolean creationSuccess = downloadDir.toFile().mkdir();
-                if(!creationSuccess) {
-                    throw new IllegalStateException("Could not create download directory");
-                }
-            }
-
-            for (Locator pdfLink : pdfLinks) {
-                Download download = page.waitForDownload(pdfLink::click);
-                download.saveAs(Paths.get(downloadDir.toString(), download.suggestedFilename()));
-            }
-
-            browser.close();
-        }
+        PageScraping.scrapePdfs("https://www.ffg.at/Breitband2030/GigaApp/2AS");
     }
 }
---
+++ scraping-demo/src/main/java/org/example/PageScraping.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageScraping {
+
+    public static List<Path> scrapePdfs(String url) {
+        List<Path> savedFiles = new ArrayList<>();
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate(url);
+
+            // Extract all PDF links
+            List<Locator> pdfLinks = new ArrayList<>();
+            page.locator("article a").all().forEach(link -> {
+                String linkDestination = link.getAttribute("href");
+                if(linkDestination == null) {
+                    return;
+                }
+
+                if(linkDestination.startsWith("https://fdoc.ffg.at/") || linkDestination.endsWith(".pdf")) {
+                    pdfLinks.add(link);
+                }
+            });
+
+            // Make dir download in user.dir if not exists
+            Path downloadDir = Paths.get(System.getProperty("user.dir"), "download");
+            if (!downloadDir.toFile().exists()) {
+                boolean creationSuccess = downloadDir.toFile().mkdir();
+                if(!creationSuccess) {
+                    throw new IllegalStateException("Could not create download directory");
+                }
+            }
+
+            for (Locator pdfLink : pdfLinks) {
+                Download download = page.waitForDownload(pdfLink::click);
+                Path savePath = Paths.get(downloadDir.toString(), download.suggestedFilename());
+                download.saveAs(savePath);
+                savedFiles.add(savePath);
+            }
+
+            browser.close();
+        }
+
+        return savedFiles;
+    }
+
+}
