2f57baadaa1d901c94730400368e2cbbfe6c42be
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-01-27 17:31:59 +0100

    re-add scraping

---
+++ scraping/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.flywaydb.flyway") version "11.1.0"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.microsoft.playwright:playwright:1.43.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("org.flywaydb:flyway-core:11.1.0")
+    implementation("org.flywaydb:flyway-database-postgresql:11.1.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.flywaydb:flyway-database-postgresql:11.1.0")
+    }
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/grantscout-scraping"
+    user = "gs"
+    password = "Password12345678"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
