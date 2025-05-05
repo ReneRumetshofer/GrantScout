941361d4ce36a7eef0af471b914f6f119dc6969e
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-05-05 20:57:47 +0200

    add skeleton for parsing use case

---
+++ backend/src/main/java/at/fhtw/grantscout/core/ParseCallUseCase.java
@@ -0,0 +1,12 @@
+package at.fhtw.grantscout.core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParseCallUseCase {
+
+    public void parseCall(Long scrapedCallId) {
+
+    }
+
+}
--- backend/src/main/java/at/fhtw/grantscout/in/eventlistener/CallScrapedEventListener.java
+++ backend/src/main/java/at/fhtw/grantscout/in/eventlistener/CallScrapedEventListener.java
@@ -1,5 +1,6 @@
 package at.fhtw.grantscout.in.eventlistener;
 
+import at.fhtw.grantscout.core.ParseCallUseCase;
 import at.fhtw.grantscout.core.events.CallScrapedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -7,8 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class CallScrapedEventListener implements ApplicationListener<CallScrapedEvent> {
 
+    private final ParseCallUseCase parseCallUseCase;
+
+    public CallScrapedEventListener(ParseCallUseCase parseCallUseCase) {
+        this.parseCallUseCase = parseCallUseCase;
+    }
+
     @Override
     public void onApplicationEvent(CallScrapedEvent event) {
-
+        parseCallUseCase.parseCall(event.getScrapedCallId());
     }
 }
